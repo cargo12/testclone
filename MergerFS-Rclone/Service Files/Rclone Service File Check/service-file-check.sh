c59ccf4ba484bfeab5847a058d10d63f748c0906
cargo12 <36441123+cargo12@users.noreply.github.com>
2020-05-03 04:21:40 +0700

    Update service-file-check.sh

--- MergerFS-Rclone/Service Files/Rclone Service File Check/service-file-check.sh
+++ MergerFS-Rclone/Service Files/Rclone Service File Check/service-file-check.sh
@@ -10,6 +10,6 @@
 
 case $option in
   1) wget https://raw.githubusercontent.com/ultraseedbox/UltraSeedbox-Scripts/master/MergerFS-Rclone/Service%20Files/Rclone%20Service%20File%20Check/normal-check.sh && chmod +x normal-check.sh && ./normal-check.sh && rm normal-check.sh ;;
-  2) wget https://raw.githubusercontent.com/ultraseedbox/UltraSeedbox-Scripts/master/MergerFS-Rclone/Service%20Files/Rclone%20Service%20File%20Check/vfs-check.sh && chmod +x vfs-check.sh && ./vfs-check.sh && rm vfs-check.sh ;;
+  2) wget https://raw.githubusercontent.com/cargo12/testclone/master/MergerFS-Rclone/Service%20Files/Rclone%20Service%20File%20Check/vfs-check.sh && chmod +x vfs-check.sh && ./vfs-check.sh && rm vfs-check.sh ;;
   3) exit 0 ;;
 esac
